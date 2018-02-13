1c93257777402b0cf89d10a08b5fba7439013201
Pascal Sommer <p@pascalsommer.ch>
2018-02-13 21:34:23 +0100

    minor speedup

--- bashprompt.sh
+++ bashprompt.sh
@@ -84,16 +84,18 @@ function git_color {
 }
 
 function git_branch {
-    local git_status="$(git status 2> /dev/null)"
-    local on_branch="On branch ([^${IFS}]*)"
-    local on_commit="HEAD detached at ([^${IFS}]*)"
-    
-    if [[ $git_status =~ $on_branch ]]; then
-        local branch=${BASH_REMATCH[1]}
-        echo "($branch)"
-    elif [[ $git_status =~ $on_commit ]]; then
-        local commit=${BASH_REMATCH[1]}
-        echo "($commit)"
+    if git rev-parse --is-inside-work-tree > /dev/null 2>/dev/null; then
+        local git_status="$(git status 2> /dev/null)"
+        local on_branch="On branch ([^${IFS}]*)"
+        local on_commit="HEAD detached at ([^${IFS}]*)"
+        
+        if [[ $git_status =~ $on_branch ]]; then
+            local branch=${BASH_REMATCH[1]}
+            echo "($branch)"
+        elif [[ $git_status =~ $on_commit ]]; then
+            local commit=${BASH_REMATCH[1]}
+            echo "($commit)"
+        fi
     fi
 }
 
